5071b31131dbf53b705efe8a08d3afd21ae5e7e0
peter nadel <peter.m.nadel@gmail.com>
2024-01-30 08:58:53 -0500

    tooltips

--- pages/zero_shot_codes_test.py
+++ pages/zero_shot_codes_test.py
@@ -8,14 +8,43 @@ with open('./data/dod_sec_zs_example.p', 'rb') as f:
     data = pickle.load(f)
 
 color_mapping = {
-    "Liberalism":"cornflowerblue", 
-    "Constructivism":"orange", 
-    "Realism":"lightgrey", 
-    "Cyber Persistence":"darkorange", 
-    "Policy Engineering Tasks":"green"
+    "Academic Interaction":"gold",
+    "Liberalism":"darkgreen", 
+    "Constructivism":"darkblue", 
+    "Realism":"darkred", 
+    "Cyber Persistence":"saddlebrown", 
+    "Policy Engineering Tasks":"teal"
 }
 
-mark_tag = "<mark style='background-color:{color}'>{line}</mark>"
+css = """<style>
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 5px;
+
+  /* Position the tooltip */
+  position: left;
+  z-index: 1;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+
+</style>"""
+
+st.markdown(css, unsafe_allow_html=True)
+
+mark_tag = """<mark class='tooltip' style='background-color:{color}'>{line}<span class="tooltiptext">{tag}</span></mark>"""
 
 for d in data:
     escape_line = d[0].replace('-', '\-')
@@ -23,6 +52,7 @@ for d in data:
         color = d[1].split('_')[0]
         c = re.sub(r'\{color\}', color_mapping[color], mark_tag)
         l = re.sub(r'\{line\}', escape_line, c)
-        st.markdown(l, unsafe_allow_html=True)
+        t = re.sub(r'\{tag\}', color, l)
+        st.markdown(t, unsafe_allow_html=True)
     else:
         st.write(escape_line)
